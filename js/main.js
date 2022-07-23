06ce647b1b5f775fb3bcdfd8744c9df7fdc03217
KsuhaBur <burlakova.ka@students.dvfu.ru>
2022-07-24 04:09:18 +1000

    day #1

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const buttonOut = document.querySelector('.button-out')
+const userName = document.querySelector('.user-name')
+const closeAuth = document.querySelector('.close-auth')
+const loginInForm = document.getElementById('logInForm')
+const inputLogin = document.getElementById('login')
+const inputPassword = document.getElementById('password')
+
+const login = (user) => {
+    buttonAuth.style.display = 'none';
+    buttonOut.style.display = 'flex';
+    userName.style.display = 'flex';
+
+    userName.textContent = user.login;
+    modalAuth.style.display = 'none';
+}
+
+const logout = () => {
+    buttonOut.style.display = 'none';
+    userName.style.display = 'none';
+    buttonAuth.style.display = 'flex';
+
+    localStorage.removeItem('user')
+}
+
+
+buttonAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'flex';
+})
+
+closeAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'none';
+})
+
+buttonOut.addEventListener('click', () => {
+    logout();
+})
+
+
+loginInForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (inputLogin.value) {
+        const user = {
+            login: inputLogin.value,
+            password: inputPassword.value
+        }
+
+        localStorage.setItem('user', JSON.stringify(user));
+        login(user);
+    } else {
+        alert('Не введен логин');
+    }
+
+
+})
+
+if (localStorage.getItem('user')) {
+    login(JSON.parse(localStorage.getItem('user')));
+}
